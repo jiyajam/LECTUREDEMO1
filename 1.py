693f2d4c1d1004afb890d387dff71dc8bd37f9e0
jiyajameela <jiyak@metropolia.fi>
2024-08-22 10:38:38 +0300

    iitial commit , variables are defined .

---
+++ 1.py
@@ -0,0 +1,11 @@
+intvariable = 4
+floatVariable =4.5
+stringVariable = "Software 1 is fun"
+
+print (intvariable)
+print(floatVariable)
+print(stringVariable)
+
+print (type(intvariable))
+print (type(floatVariable))
+print (type(stringVariable))
